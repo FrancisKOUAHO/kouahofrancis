5a4fea0d41c49d5af8c163795c53d88861d9ddd6
FrancisKOUAHO <kouahofrancis@gmail.com>
2022-09-10 20:23:27 +0200

    setup: (setup + tailwind && eslintrc)

--- next.config.js
+++ next.config.js
@@ -1,21 +1,7 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
-
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  trailingSlash: true,
-  webpackDevMiddleware: config => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300
-    }
-    return config
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')]
-  }
+  swcMinify: true
 }
 
 module.exports = nextConfig
-
