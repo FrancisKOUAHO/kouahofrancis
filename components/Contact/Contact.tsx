6c381ccd0ea4dc57126dc27314fbc36a6021d0c4
FrancisKOUAHO <kouahofrancis@gmail.com>
2022-09-11 15:30:32 +0200

    fix: fixed Contact && Presentation

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -7,9 +7,10 @@ const Contact: NextPage = () => {
             <h2
                 className="mb-8 sm:mb-10 text-4xl text-white text-center fadeup-enter fadeup-enter-done"
                 style={{transitionDelay: "50ms"}}>Contact</h2>
-            <div className="flex flex-col items-center text-white gap-4"><p>Feel free to contact me by email or through
-                my social
-                networks.</p>
+            <div className="flex flex-col items-center text-white gap-4">
+                <p>
+                    N'hésitez pas à me contacter par courriel ou via mes réseaux sociaux.
+                </p>
                 <div className="flex gap-2 flex-wrap">
                     <SocialNetwork/>
                 </div>
--- components/Presentation/Presentation.tsx
+++ components/Presentation/Presentation.tsx
@@ -8,14 +8,16 @@ const Presentation: NextPage = () => {
                 <img src="/Logo_Portfolio_Francis_Jaune.png" alt="" className="Profile_pointBg__1Z7Zk w-20 h-20"
                      style={{opacity: "0.2"}}/>
                 <p className="text-amber-200 text-2xl font-bold relative right-2 fadeup-enter fadeup-enter-done"
-                   style={{transitionDelay: "300ms"}}>Hey there! I&lsquo;m -
+                   style={{transitionDelay: "300ms"}}>Bonjour à tous ! Je suis -
                 </p>
                 <h1 className="header-h1-fluid font-bold text-gradient-primary fadeup-enter fadeup-enter-done"
-                    style={{transitionDelay:"400ms"}}>Francis KOUAHO
+                    style={{transitionDelay: "400ms"}}>Francis KOUAHO
                 </h1>
                 <p className="header-h2-fluid text-textSecondary sm:mt-4 fadeup-enter fadeup-enter-done"
-                   style={{transitionDelay:"500ms"}}><b className="text-textPrimary">FrontEnd Software Engineer.</b> Self-taught
-                    since I was 16 years old, then apprenticed in a company  </p>
+                   style={{transitionDelay: "500ms"}}><b className="text-textPrimary">
+                    Développeur FrontEnd.</b> Autodidacte
+                    depuis l&rsquo;âge de 15 ans, Et actuellement apprenti développeur FrontEnd chez <a href="https://www.welcometothejungle.com/fr/companies/mybestpro">MyBestPro</a>.
+                </p>
             </div>
             <SocialNetwork/>
         </div>
